bceec1cd949dbf5bae42bee075459104a3b44205
arun <arun.kr.reddy@gmail.com>
2025-12-08 22:22:25 +0530

    Format main.cpp

--- Code/Src/main.cpp
+++ Code/Src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main(int argc, char const* argv[]) {
-  std::cout << "hello world" << std::endl;
-  return 0;
+int main(int argc, char const *argv[])
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
 }
